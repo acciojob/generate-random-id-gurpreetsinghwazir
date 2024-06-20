2ea2e18baaa91843ccbd352e2e466b89609575de
Dragon_Fly <143097451+gurpreetsinghwazir@users.noreply.github.com>
2024-06-20 23:54:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function makeid(l) {
   // write your code here
+let last = ""
+	for (let i = 0; i < l; i++) {
+		let num = Math.floor(Math.random() * 100)
+		let st = String.fromCharCode(num);
+		last = last + st ;
+	}
+	console.log(last)
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+
